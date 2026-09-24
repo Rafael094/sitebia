94baa61afbda5c8047a0ecfb22f08deb64a66b47
Rafael Moura <rafaelmoura094@gmail.com>
2026-09-23 22:54:57 -0300

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,37 @@ export const metadata: Metadata = {
     template: SITE.title("%s")
   },
   description: SITE.description,
+  icons: {
+    icon: [
+      {
+        url: "/images/Favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png"
+      },
+      {
+        url: "/images/Favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png"
+      },
+      {
+        url: "/images/Favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        url: "/images/Favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ],
+    apple: [
+      {
+        url: "/images/Favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png"
+      }
+    ]
+  },
   openGraph: {
     title: SITE.title(),
     description: SITE.description,
